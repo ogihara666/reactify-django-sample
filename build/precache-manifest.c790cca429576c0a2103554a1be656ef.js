fc9f749fe601427a44eaa9c830e9026351cc1a46
ogipochi <ogihara@coly.info>
2018-10-04 15:46:54 +0900

    replace function component

---
+++ build/precache-manifest.c790cca429576c0a2103554a1be656ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6d6574ce9b69a97d3b24",
+    "url": "/static/js/main.6d6574ce.chunk.js"
+  },
+  {
+    "revision": "b8a4aa0272c48f16e6d2",
+    "url": "/static/js/1.b8a4aa02.chunk.js"
+  },
+  {
+    "revision": "6d6574ce9b69a97d3b24",
+    "url": "/static/css/main.e7eea197.chunk.css"
+  },
+  {
+    "revision": "036948b0ca309151bd7e0f4ce39a00c8",
+    "url": "/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.328149165bf5641573a1d49ebc88fda3.js"
+  "/precache-manifest.c790cca429576c0a2103554a1be656ef.js"
 );
 
 workbox.clientsClaim();
